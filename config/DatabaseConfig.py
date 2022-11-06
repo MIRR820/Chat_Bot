db7cc0c032cd875040af856253a3d5a4c0f15ec6
MIRR820 <81572157+MIRR820@users.noreply.github.com>
2022-11-06 12:25:35 +0900

    Refactor : 변수명 오타 수정

--- config/DatabaseConfig.py
+++ config/DatabaseConfig.py
@@ -1,7 +1,7 @@
 DB_HOST = "127.0.0.1"
 DB_USER = "root"
-DB_PASSSWORD = "admin"
+DB_PASSWORD = "admin"
 DB_NAME = "homestead"
 
 def DatabaseConfig():
-    global DB_HOST, DB_USER, DB_PASSSWORD, DB_NAME
+    global DB_HOST, DB_USER, DB_PASSWORD, DB_NAME
